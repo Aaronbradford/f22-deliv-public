7d4b506dd0c4943c3f6891e461a38a2266711b5c
Aaron Bradford <abradford@college.harvard.edu>
2023-02-04 20:02:13 -0500

    Fixed commit?

---
+++ src/components/WeatherModal.js
@@ -0,0 +1,58 @@
+/* Adding a weather section of the pagethat users can use to see the 
+weather of a specific area – could be helpful in the case of 
+understanding certain climates. */
+
+import React, { useState } from 'react';
+
+const api = {
+  key: process.env.REACT_APP_API_KEY,
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+export default function WeatherModal() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&appid=${api.key}&units=metric`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search for the weather in a city and country combo eg: Cambridge, US..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            {/* <div className="date">{dateBuilder(new Date())}</div> */}
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
+        ) : ('')}
+      </main>
+    </div>
+  );
+}
